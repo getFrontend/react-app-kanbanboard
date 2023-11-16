202a1fa5da0e9de98d1ee543810bcb0413f808c0
Sergey UP <sergey10goals@gmail.com>
2023-11-16 23:38:09 +0200

    add custom hook useTaskCollection

---
+++ src/hooks/useTaskCollection.ts
@@ -0,0 +1,47 @@
+import { useLocalStorage } from 'usehooks-ts';
+import { ColumnType } from '../utils/enums';
+import { TaskModel } from '../utils/models';
+import { v4 as uuidv4 } from 'uuid';
+
+function useTaskCollection() {
+  return useLocalStorage<{
+    [key in ColumnType]: TaskModel[];
+  }>(
+    'tasks', {
+    'Hot tasks': [
+      {
+        id: uuidv4(),
+        column: ColumnType.HOT_TASKS,
+        title: 'Task 1',
+        color: 'orange.300'
+      }
+    ],
+    'To do': [
+      {
+        id: uuidv4(),
+        column: ColumnType.HOT_TASKS,
+        title: 'Task 2',
+        color: 'blue.300'
+      }
+    ],
+    'In work': [
+      {
+        id: uuidv4(),
+        column: ColumnType.HOT_TASKS,
+        title: 'Task 3',
+        color: 'yellow.300'
+      }
+    ],
+    'Done': [
+      {
+        id: uuidv4(),
+        column: ColumnType.HOT_TASKS,
+        title: 'Task 4',
+        color: 'green.300'
+      }
+    ]
+  }
+  );
+}
+
+export default useTaskCollection;
