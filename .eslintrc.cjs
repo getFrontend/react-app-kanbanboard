2b3c4baeb396c13f71332fb9693109db4f3b2f97
Sergey UP <sergey10goals@gmail.com>
2023-11-16 21:27:24 +0200

    update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,9 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'semi': ['error', 'always', { 'omitLastInOneLineBlock': false }],
+    'comma-dangle': ['error', 'never'],
+    quotes: ['error', 'single'],
+    'react/prop-types': [0],
   },
 }
