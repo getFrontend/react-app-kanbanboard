fc9da482e59847d468d421d423ea453c8a9f2ed8
Sergey UP <sergey10goals@gmail.com>
2023-11-17 23:29:52 +0200

    last update

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -44,45 +44,45 @@ function Column({ column }: { column: ColumnType }) {
 
   return (
     <Box>
-    <Heading fontSize="md" mb={4} letterSpacing="wide">
-      <Badge
-        px={2}
-        py={1}
+      <Heading fontSize="md" mb={4} letterSpacing="wide">
+        <Badge
+          px={2}
+          py={1}
+          rounded="lg"
+          colorScheme={ColumnColorScheme[column]}
+        >
+          {column}
+        </Badge>
+      </Heading>
+      <IconButton
+        size="xs"
+        w="full"
+        color={useColorModeValue('gray.500', 'gray.400')}
+        bgColor={useColorModeValue('gray.100', 'gray.700')}
+        _hover={{ bgColor: useColorModeValue('gray.200', 'gray.600') }}
+        py={2}
+        variant="solid"
+        onClick={addEmptyTask}
+        colorScheme="black"
+        aria-label="add-task"
+        icon={<AddIcon />}
+      />
+      <Stack
+        ref={dropRef}
+        direction={{ base: 'row', md: 'column' }}
+        h={{ base: 300, md: 600 }}
+        p={4}
+        mt={2}
+        spacing={4}
+        bgColor={useColorModeValue('gray.50', 'gray.900')}
         rounded="lg"
-        colorScheme={ColumnColorScheme[column]}
+        boxShadow="md"
+        overflow="auto"
+        opacity={isOver ? 0.85 : 1}
       >
-        {column}
-      </Badge>
-    </Heading>
-    <IconButton
-      size="xs"
-      w="full"
-      color={useColorModeValue('gray.500', 'gray.400')}
-      bgColor={useColorModeValue('gray.100', 'gray.700')}
-      _hover={{ bgColor: useColorModeValue('gray.200', 'gray.600') }}
-      py={2}
-      variant="solid"
-      onClick={addEmptyTask}
-      colorScheme="black"
-      aria-label="add-task"
-      icon={<AddIcon />}
-    />
-    <Stack
-      ref={dropRef}
-      direction={{ base: 'row', md: 'column' }}
-      h={{ base: 300, md: 600 }}
-      p={4}
-      mt={2}
-      spacing={4}
-      bgColor={useColorModeValue('gray.50', 'gray.900')}
-      rounded="lg"
-      boxShadow="md"
-      overflow="auto"
-      opacity={isOver ? 0.85 : 1}
-    >
-      {ColumnTasks}
-    </Stack>
-  </Box>
+        {ColumnTasks}
+      </Stack>
+    </Box>
   );
 }
 
