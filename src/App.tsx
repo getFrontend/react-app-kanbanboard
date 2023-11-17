9e64f9ba53fa8f2b633812c6f3bfdd5be004e7b8
Sergey UP <sergey10goals@gmail.com>
2023-11-17 11:46:43 +0200

    add Column to SimpleFrid & add DndProvide

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 // import { useState } from 'react';
 import { Heading, Container, SimpleGrid } from '@chakra-ui/react';
+import Column from './components/Column/Column';
+import { ColumnType } from './utils/enums';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-    <>
+    <main>
       <Heading
         fontSize={{ base: '4xl', sm: '5xl', md: '6xl' }}
         fontWeight="bold"
@@ -16,19 +20,24 @@ function App() {
       >
         Just one more usefull Kanban Board
       </Heading >
-      <Container
-        maxWidth="container.lg"
-        px={4}
-        py={10}
-      >
-        <SimpleGrid
-          columns={{ base: 1, md: 4 }}
-          spacing={{ base: 16, m: 4 }}
+      <DndProvider backend={HTML5Backend}>
+        <Container
+          maxWidth="container.lg"
+          px={4}
+          py={10}
         >
-
-        </SimpleGrid>
-      </Container>
-    </>
+          <SimpleGrid
+            columns={{ base: 1, md: 4 }}
+            spacing={{ base: 16, m: 4 }}
+          >
+            <Column column={ColumnType.HOT_TASKS} />
+            <Column column={ColumnType.TO_DO} />
+            <Column column={ColumnType.IN_PROGRESS} />
+            <Column column={ColumnType.DONE} />
+          </SimpleGrid>
+        </Container>
+      </DndProvider>
+    </main>
   );
 }
 
