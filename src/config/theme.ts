319836cff92f70afae7f67c7b3142e14c3ffa33c
Sergey UP <sergey10goals@gmail.com>
2023-11-18 22:07:31 +0200

    update bg color

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -4,12 +4,12 @@ import './../index.css'
 const theme = extendTheme({
   fonts: {
     heading: `'Nunito Sans', sans-serif`,
-    body: `'Roboto Condensed', sans-serif`
+    body: `'Roboto', sans-serif`
   },
   styles: {
     global: (props: { colorMode: string }) => ({
       body: {
-        bg: props.colorMode === 'dark' ? 'dark.800' : '#FAF3F0'
+        bg: props.colorMode === 'dark' ? 'dark.800' : '#EEE3CB'
       }
     })
   }
