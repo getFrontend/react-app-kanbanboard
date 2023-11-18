a7e0096c403830d0bad2725e77df9dafb52936a5
Sergey UP <sergey10goals@gmail.com>
2023-11-18 20:59:25 +0200

    add fonts

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
+import './../index.css'
 
 const theme = extendTheme({
+  fonts: {
+    heading: `'Nunito Sans', sans-serif`,
+    body: `'Roboto Condensed', sans-serif`
+  },
   styles: {
     global: (props: { colorMode: string }) => ({
       body: {
